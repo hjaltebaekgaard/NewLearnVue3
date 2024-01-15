883ea914b7fa4d7d453b007fec10c3ea6f95837e
Hjalte <hjaltebaekgaard@hotmai.com>
2024-01-16 00:44:18 +0100

    Step 1 looking into composition api

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,38 @@
-<script setup>
+<script>
 import TheWelcome from '../components/TheWelcome.vue'
+import { onMounted, ref } from "vue";
+
+export default {
+  components: {
+    TheWelcome
+  },
+
+  setup() {
+    onMounted(() => {
+      alert("I am mounted")
+    });
+
+    let message = ref("Hello World!!")
+
+    setTimeout(() => {
+      message.value = "Time's up!"
+    }, 3000)
+
+    return  {
+      message
+    }
+  }
+ 
+}
 </script>
 
 <template>
   <main>
     <TheWelcome />
+
+    <p>{{ message }}</p>
+    <p>
+      <input type="text" v-model="message" />
+    </p>
   </main>
 </template>
